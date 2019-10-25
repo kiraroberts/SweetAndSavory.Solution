7aaefa7574a1e81a60cbc1603e7305044f9d0551
Kira Roberts <rosehill1788@protonmail.com>
2019-10-25 09:58:04 -0700

    add HomeController.cs and TreatsController.cs, fix typo in SweetAndSavoryContext.cs

--- SweetAndSavory/Models/SweetAndSavoryContext.cs
+++ SweetAndSavory/Models/SweetAndSavoryContext.cs
@@ -6,7 +6,7 @@ namespace SweetAndSavory.Models
     {
         public virtual DbSet<Flavor> Flavors { get; set; }
         public DbSet<Treat> Treats { get; set; }
-        public DbSet<FlavorTreat> FlavorTreats { get; set; }
+        public DbSet<FlavorTreat> FlavorTreat { get; set; }
 
         public SweetAndSavoryContext(DbContextOptions options) : base(options) { }
     }
